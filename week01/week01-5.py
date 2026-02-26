ffcc531f00c81777b48bca985ff49d89adc14c55
IanChou726 <ianchou950726@gmail.com>
2026-02-26 16:42:59 +0800

    Week01

---
+++ week01/week01-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        N =len(nums)
+        preSum = [1]
+        postSum = [1]
+        for i in range(N):
+            preSum.append(preSum[-1]*nums[i])
+            postSum.append(postSum[-1]*nums[N-1-i])
+        ans = []
+        for i in range(N):
+            ans.append(preSum[i]*postSum[N-1-i])
+        return ans
